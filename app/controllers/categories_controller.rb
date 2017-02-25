d8c330e40c8ff05771b3398c07efd5dfc9627685
bruno <bruno.basr.alves@gmail.com>
2017-02-25 18:31:53 -0300

    Category link

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,24 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    @jobs = Job.all
+    @companies = Company.all
+  end
   def new
     @category = Category.new
+    @jobs = Job.all
+    @companies = Company.all
   end
   def create
+    @jobs = Job.all
     categories_params = params.require(:category).permit(:name)
     @category = Category.new
     @category.save
     redirect_to @category
+  end
+  def show
+    @jobs = Job.all
+    @category = Category.find(params[:id])
   end
 
 
